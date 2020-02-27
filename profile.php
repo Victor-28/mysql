18e7cb298db32fd21003193f06ae606463ae99a8
Victor-28 <victorakintola@gmail.com>
2020-02-27 10:10:15 +0100

    Select form student

---
+++ profile.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+
+class profileController
+{
+
+
+    public function render()
+    {
+        $newConnection = new Connection();
+        $newConnection = $newConnection->openConnection();
+        //the get refers to the ?+name of $_GET in the url
+        $userId = $_GET['user'];
+        $stmt = $newConnection->prepare('SELECT * FROM students WHERE id=:id');
+        $stmt->execute([ $userId]);
+        $user = $stmt->fetch();
+        require 'View/profile.php';
+    }
+
+}
